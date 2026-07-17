27f8717ac46dd84e5fa23c53759a7eb98a66c17d
snksw <snoo999@naver.com>
2026-07-17 15:21:10 +0900

    Initial commit

---
+++ src/main/java/org/kidscafe/kindy/KindyApplication.java
@@ -0,0 +1,13 @@
+package org.kidscafe.kindy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KindyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KindyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/kidscafe/kindy/KindyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kidscafe.kindy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KindyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
