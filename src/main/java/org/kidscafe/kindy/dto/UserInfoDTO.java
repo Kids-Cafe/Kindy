6104bdbee19dc19fb01ffa9fd3ecdf784fc14a50
snksw <snoo999@naver.com>
2026-07-25 05:00:57 +0900

    chore: Add @Data to UserInfoDTO

--- src/main/java/org/kidscafe/kindy/dto/UserInfoDTO.java
+++ src/main/java/org/kidscafe/kindy/dto/UserInfoDTO.java
@@ -1,35 +1,21 @@
 package org.kidscafe.kindy.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
-@Getter
-@Setter
+@Data
 @JsonInclude(JsonInclude.Include.NON_DEFAULT)
 public class UserInfoDTO {
-
     private String userId;
-
     private String userName;
-
     private String password;
-
     private String email;
-
     private String addr1;
-
     private String addr2;
-
     private String regId;
-
     private String regDt;
-
     private String chgId;
-
     private String chgDt;
-
     private String existsYn;
-
     private int authNumber;
 }
