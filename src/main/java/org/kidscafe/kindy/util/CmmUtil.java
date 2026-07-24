9c7841e9a51ad8477446acc0c069bf0de384da7e
snksw <snoo999@naver.com>
2026-07-25 04:39:06 +0900

    chore: remove redundant code

--- src/main/java/org/kidscafe/kindy/util/CmmUtil.java
+++ src/main/java/org/kidscafe/kindy/util/CmmUtil.java
@@ -1,7 +1,5 @@
 package org.kidscafe.kindy.util;
 
-import java.util.Objects;
-
 public class CmmUtil {
 
     public static String nvl(String str, String chg_str) {
@@ -12,26 +10,5 @@ public class CmmUtil {
     public static String nvl(String str) {
 
         return nvl(str, "");
-    }
-
-    public static String checked(String str, String com_str) {
-
-        return Objects.equals(str, com_str) ? " checked" : "";
-    }
-
-    public static String checked(String[] str, String com_str) {
-        if (str == null) return ""; // null 방어
-
-        for (String s : str) {
-            if (Objects.equals(s, com_str)) {
-                return " checked";
-            }
-        }
-        return "";
-    }
-
-    public static String select(String str, String com_str) {
-
-        return Objects.equals(str, com_str) ? " selected" : "";
     }
 }
